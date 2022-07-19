e087ed035c6eff5093c89a0b65181cf242ebc615
Patrick Pfenning <patrick.pfenning@uptake.com>
2022-07-19 19:31:30 -0400

    more checks when getting data

--- helper_scripts/get_data.sh
+++ helper_scripts/get_data.sh
@@ -5,15 +5,26 @@ echo ""
 echo "Unzip kaggle set..."
 unzip crimes-in-chicago.zip
 echo ""
+echo "Verify word count for CSVs..."
+wc -l ./*.csv
+echo ""
 echo "Combine CSVs..."
-awk '(NR == 1) || (FNR > 1)' ./*.csv > Chicago_Crimes_Dirty.csv
+header=$(head -n 1 Chicago_Crimes_2001_to_2004.csv)
+header="num${header}"
+echo "${header}" > Chicago_Crimes_Dirty.out && tail -n+2 -q ./*.csv >> Chicago_Crimes_Dirty.out
+echo ""
+echo "Verify word count for combined..."
+wc -l ./*.out
 echo ""
 echo "Removing null years..."
-awk '!($19 == "")' Chicago_Crimes_Dirty.csv > Chicago_Crimes.csv
+awk -F"," '!($19 == "")' Chicago_Crimes_Dirty.out > Chicago_Crimes.csv
+echo ""
+echo "Verify word count for cleaned data..."
+wc -l Chicago_Crimes.csv
 echo ""
 echo "Zipping single csv..."
 zip Chicago_Crimes.zip Chicago_Crimes.csv
 echo ""
 echo "Removing unwanted data..."
-rm ./*.csv crimes-in-chicago.zip
+rm ./*.csv ./*.out crimes-in-chicago.zip
 echo ""
